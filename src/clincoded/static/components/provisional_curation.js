e133a08ab9f691aba3d34f69aff1784edf832b3b
Kang Liu <kgliu0101@gmail.com>
2015-09-13 22:18:58 -0700

    multiple variants in family

--- src/clincoded/static/components/provisional_curation.js
+++ src/clincoded/static/components/provisional_curation.js
@@ -161,81 +161,50 @@ var ProvisionalCuration = React.createClass({
             stateObj.assessed_patho = variantsList;
 
             var annotations = stateObj.gdm.annotations;
-            var allFamilies = []; // collect all families in the gdm
-            var allIndividuals = [];
+            var familiesCollected = []; // collect all families in the gdm
+            var individualsCollected = [];
             for (var i in annotations) {
                 if (annotations[i].groups) {
                     var groups = annotations[i].groups;
                     for (var j in groups) {
                         if (groups[j].familyIncluded) {
-                            array_append(allFamilies, groups[j].familyIncluded, annotations[i].article);
+                            filter(familiesCollected, groups[j].familyIncluded, annotations[i].article, variantIdList);
                         }
                         if (groups[j].individualIncluded) {
-                            array_append(allIndividuals, groups[j].individualIncluded, annotations[i].article);
+                            filter(individualsCollected, groups[j].individualIncluded, annotations[i].article, variantIdList);
                         }
                     }
                 }
                 if (annotations[i].families) {
-                    array_append(allFamilies, annotations[i].families, annotations[i].article);
+                    filter(familiesCollected, annotations[i].families, annotations[i].article, variantIdList);
                 }
                 if (annotations[i].individuals) {
-                    array_append(allIndividuals, annotations[i].individuals, annotations[i].article);
+                    filter(individualsCollected, annotations[i].individuals, annotations[i].article, variantIdList);
                 }
             }
-            stateObj.allFamilies = allFamilies;
 
             var articleCollected = [];
             var year = new Date();
             var earliest = year.getFullYear();
-            var familiesCollected = [];
-            var familyIdPicked = [];
-            for (var i in allFamilies) {
-                var family = allFamilies[i]['evidenceObj'];
-                var article = allFamilies[i]['article'];
-                if (family.segregation) {
-                    var seg = family.segregation;
-                    if (seg.variants) {
-                        var variants = seg.variants;
-                        for (var j in variants) {
-                            if (in_array(variants[j].uuid, variantIdList)) {
-                                familiesCollected.push({ "family":family.uuid, "variant":variants[j].uuid, "pmid":article.pmid, "year":article.date });
-                                if (!in_array(family.uuid, familyIdPicked)) {
-                                    familyIdPicked.push(family.uuid);
-                                }
-                                if (!in_array(article.pmid, articleCollected)) {
-                                    articleCollected.push(article.pmid);
-                                    earliest = setEarliestYear(earliest, article.date);
-                                }
-                            }
-                        }
-                    }
+            for (var i in familiesCollected) {
+                if (!in_array(familiesCollected[i].pmid, articleCollected) && familiesCollected[i].pmid != '') {
+                    articleCollected.push(familiesCollected[i].pmid);
+                    earliest = setEarliestYear(earliest, familiesCollected[i].date);
                 }
             }
             stateObj.familiesCollected = familiesCollected;
-            stateObj.testStr = familyIdPicked.length + '';
 
-            var individualsCollected = [];
-            if (variantIdList.length > 0 && allIndividuals.length >0) {
-                var individualIdPicked = [];
-                for (var i in allIndividuals) {
-                    var individual = allIndividuals[i]['evidenceObj'];
-                    var article = allIndividuals[i]['article'];
-                    var variants = individual.variants;
-                    for (var j in variants) {
-                        if (in_array(variants[j].uuid, variantIdList) && !in_array(individual.uuid, individualIdPicked)) {
-                            individualsCollected.push({ "individual":individual.uuid, "variant":variants[j].uuid, "pmid":article.pmid, "year":article.date });
-                            individualIdPicked.push(individual.uuid);
-                            if (!in_array(article.pmid, articleCollected)) {
-                                articleCollected.push(article.pmid);
-                                earliest = setEarliestYear(earliest, article.date);
-                            }
-                            break;
-                        }
+            if (individualsCollected.length > 0) {
+                for (var i in individualsCollected) {
+                    if (!in_array(individualsCollected[i].pmid, articleCollected) && individualsCollected[i].pmid != '') {
+                        articleCollected.push(individualsCollected[i].pmid);
+                        earliest = setEarliestYear(earliest, individualsCollected[i].date);
                     }
                 }
             }
             stateObj.individualsCollected = individualsCollected;
             stateObj.publications = articleCollected;
+
             var currentYear = year.getFullYear();
             stateObj.years = (currentYear.valueOf() - earliest.valueOf()) + ' = ' + currentYear + ' - ' + earliest;
             var time = currentYear.valueOf() - earliest.valueOf();
@@ -249,7 +218,8 @@ var ProvisionalCuration = React.createClass({
             else {
                 timeScore = 0;
             }
-            var proband = stateObj.familiesCollected.length + stateObj.individualsCollected.length;
+
+            var proband = count_proband(familiesCollected) + count_proband(individualsCollected);
             if (proband > 18) {
                 probandScore = 7;
             }
@@ -274,18 +244,21 @@ var ProvisionalCuration = React.createClass({
             else {
                 probandScore = 0;
             }
+
             if (stateObj.finalExperimentalScore >= 6) {
                 expScore = 6;
             }
             else {
                 expScore = stateObj.finalExperimentalScore;
             }
+
             if (stateObj.publications.length >= 5) {
                 pubScore = 5;
             }
             else {
                 pubScore = stateObj.publications.length;
             }
+
             if (stateObj.finalExperimentalScore >= 6) {
                 expScore = 6;
             }
@@ -322,6 +295,9 @@ var ProvisionalCuration = React.createClass({
 
     render: function() {
         this.queryValues.gdmUuid = queryKeyValue('gdm', this.props.href);
+
+        var families = count_proband(this.state.familiesCollected);
+        var individuals = count_proband(this.state.individualsCollected);
         return (
             <div className="container">
                 <h1>Summary and Provisional Classification</h1>
@@ -332,32 +308,78 @@ var ProvisionalCuration = React.createClass({
                     <tr><td><strong>db gdm id</strong></td><td>{this.state.gdm.uuid}</td></tr>
                 </table>
                 <hr width="100%" />
-                <h3>Calculated Score / Assigned Classification: {this.state.totalScore.time + this.state.totalScore.proband + this.state.totalScore.pub + this.state.totalScore.exp} / {this.state.autoClassification}</h3>
+                <h3>Calculated Score / Assigned Classification: {
+                        this.state.totalScore.time + this.state.totalScore.proband + this.state.totalScore.pub +
+                        this.state.totalScore.exp} / {this.state.autoClassification
+                    }
+                </h3>
                 <h4>Final Exp. Score: {this.state.finalExperimentalScore} -- {this.state.totalScore.exp}</h4>
-                <h4># Proband: {this.state.familiesCollected.length + this.state.individualsCollected.length} -- {this.state.totalScore.proband}</h4>
+                <h4># Proband: {families + individuals} -- {this.state.totalScore.proband}</h4>
                 <h4># Publications: {this.state.publications.length} -- {this.state.totalScore.pub}</h4>
                 <h4># Years: {this.state.years} -- {this.state.totalScore.time}</h4>
                 <hr width="100%" />
                 <h4># Scored Experimental Evidence: {this.state.assessed_exp.length}</h4>
                 <table>
-                    <tr><td width="60px"><strong>Exp</strong></td><td width="300px"><strong>Type</strong></td><td width="100px"><strong>Unit Score</strong></td></tr>
-                    {this.state.assessed_exp.map(function(exp, i) { return <tr><td>{i}</td><td>{exp.type}</td><td align="center">{exp.score}</td></tr>; })}
+                    <tr><td width="60px"><strong>Exp</strong></td>
+                        <td width="300px"><strong>Type</strong></td>
+                        <td width="100px"><strong>Unit Score</strong></td>
+                    </tr>
+                    {this.state.assessed_exp.map(function(exp, i) { return (
+                        <tr><td>{i}</td>
+                            <td>{exp.type}</td>
+                            <td align="center">{exp.score}</td>
+                        </tr>
+                    );
+                })}
                 </table>
                 <hr width="100%" />
                 <h4># Variants assessed: {this.state.assessed_patho.length}</h4>
                 <table>
-                    <tr><td width="300px"><strong>Pathogenicity</strong></td><td width="300px"><strong>Variant</strong></td><td width="100px"><strong>Assessment</strong></td><td width="300px"><strong>By</strong></td></tr>
-                    {this.state.assessed_patho.map(function(variant) { return <tr><td>{variant.patho}</td><td>{variant.variant}</td><td>{variant.value}</td><td>{variant.assessedBy}</td></tr>; })}
+                    <tr><td width="300px"><strong>Pathogenicity</strong></td>
+                        <td width="300px"><strong>Variant</strong></td>
+                        <td width="100px"><strong>Assessment</strong></td>
+                        <td width="300px"><strong>By</strong></td>
+                    </tr>
+                    {this.state.assessed_patho.map(function(variant) { return (
+                        <tr><td>{variant.patho}</td>
+                            <td>{variant.variant}</td>
+                            <td>{variant.value}</td>
+                            <td>{variant.assessedBy}</td>
+                        </tr>
+                    );
+                })}
                 </table>
-                <h4># Variants Associated to Family: {this.state.familiesCollected.length}; # Proband Counted in Families: {this.state.testStr}</h4>
+                <h4># Family: {families}</h4>
                 <table>
-                    <tr><td width="300px"><strong>Family</strong></td><td width="300px"><strong>Variant</strong></td><td width="100px"><strong>PMID</strong></td><td width="300px"><strong>Pub Year</strong></td></tr>
-                    {this.state.familiesCollected.map(function(item) { return <tr><td>{item.family}</td><td>{item.variant}</td><td>{item.pmid}</td><td>{item.year}</td></tr>; })}
+                    <tr><td width="300px"><strong>Family</strong></td>
+                        <td width="300px"><strong>Variant</strong></td>
+                        <td width="100px"><strong>PMID</strong></td>
+                        <td width="300px"><strong>Pub Year</strong></td>
+                    </tr>
+                    {this.state.familiesCollected.map(function(item) { return (
+                    <tr><td>{item.evidence}</td>
+                        <td>{item.variant}</td>
+                        <td>{item.pmid}</td>
+                        <td>{item.date}</td>
+                    </tr>
+                    );
+                })}
                 </table>
-                <h4># Proband Counted in Individuals: {this.state.individualsCollected.length}</h4>
+                <h4># Individual: {individuals}</h4>
                 <table>
-                    <tr><td width="300px"><strong>Individual</strong></td><td width="300px"><strong>Variant</strong></td><td width="100px"><strong>PMID</strong></td><td width="300px"><strong>Pub Year</strong></td></tr>
-                    {this.state.individualsCollected.map(function(item) { return <tr><td>{item.individual}</td><td>{item.variant}</td><td>{item.pmid}</td><td>{item.year}</td></tr>; })}
+                    <tr><td width="300px"><strong>Individual</strong></td>
+                        <td width="300px"><strong>Variant</strong></td>
+                        <td width="100px"><strong>PMID</strong></td>
+                        <td width="300px"><strong>Pub Year</strong></td>
+                    </tr>
+                    {this.state.individualsCollected.map(function(item) { return (
+                        <tr><td>{item.evidence}</td>
+                            <td>{item.variant}</td>
+                            <td>{item.pmid}</td>
+                            <td>{item.date}</td>
+                        </tr>
+                    );
+                })}
                 </table>
             </div>
         );
@@ -385,9 +407,57 @@ var setEarliestYear = function(earliest, dateStr) {
     return earliest;
 };
 
-var array_append =function(target, branch, article) {
+var filter = function(target, branch, article, variantList) {
     for (var i in branch) {
-        target.push({"evidenceObj":branch[i], "article":article});
+        var obj = branch[i];
+        var variantIds = [];
+        var allAssessed = false;
+        if (obj['@type'][0] == 'family') {
+            if (obj.segregation) {
+                var seg = obj.segregation;
+                if (seg.variants) {
+                    var variants = seg.variants;
+                    for (var j in variants) {
+                        if (!in_array(variants[j].uuid, variantList)) {
+                            allAssessed = false;
+                            break;
+                        }
+                        else {
+                            allAssessed = true;
+                            variantIds.push(variants[j].uuid);
+                        }
+                    }
+                }
+            }
+        }
+        else if (obj['@type'][0] == 'individual') {
+            if (obj.variants) {
+                var variants = obj.variants;
+                for (var j in variants) {
+                    if (in_array(variants[j].uuid, variantList)) {
+                        variantIds.push(variants[j].uuid);
+                        allAssessed = true;
+                    }
+                }
+            }
+        }
+
+        if (allAssessed) {
+            target.push({"evidence":branch[i].uuid, "variant":variantIds[0], "pmid":article.pmid, "date": article.date});
+            if (variantIds.length > 0) {
+                target.push({"evidence":'', "variant":variantIds[1], "pmid":'', "date": ''});
+            }
+        }
     }
     return target;
+};
+
+var count_proband = function(evidenceList) {
+    var proband = 0;
+    for (var i in evidenceList) {
+        if (i === 0 || evidenceList[i].evidence !== '') {
+            proband++;
+        }
+    }
+    return proband;
 };
